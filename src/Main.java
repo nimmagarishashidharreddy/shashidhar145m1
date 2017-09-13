eefb505e6878ed6f505f9ad9daccf0eec0105448
nimmagarishashidharreddy <31915007+nimmagarishashidharreddy@users.noreply.github.com>
2017-09-13 12:59:36 +0530

    before

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+                Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		Date e = new Date();
+
+		Student[] stu = new Student[n];
+		
+	}
+
+}
